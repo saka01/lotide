53fb026f166d30df621a78122eaeb9969f2de8bf
Saka Moshood <sakamoshood@ymail.com>
2021-05-27 14:35:19 +0000

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence){
+  const count = 1
+  const result = {}
+
+  for (let i = 0; i < sentence.length; i++) {
+    if(sentence[i] !== ' '){
+   if (!result[sentence[i]]) {
+     result[sentence[i]] = 1
+   }else{
+    result[sentence[i]] += 1
+   }
+  }
+  }
+ return result;
+}
+console.log(countLetters("lighthouse in the house"));
