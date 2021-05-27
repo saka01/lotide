ff07a6ce6af823c25e9e4d633fb2d315922d6966
Saka Moshood <sakamoshood@ymail.com>
2021-05-27 16:30:38 +0000

    added and finished findKeyValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+// import {assertEqual} from './assertEqual';
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (shows, lookfor) => {
+  for (const genre in shows) {
+      if (shows[genre] === lookfor) {
+        return genre
+      }
+  }
+  return undefined
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
