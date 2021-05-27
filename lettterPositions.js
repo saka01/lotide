4509a86762cd2eb35a0479296c514f5385ada3ac
Saka Moshood <sakamoshood@ymail.com>
2021-05-27 16:13:07 +0000

    finished letterPositions.js

--- lettterPositions.js
+++ lettterPositions.js
@@ -13,9 +13,18 @@ const assertArraysEqual = function(firstArr,secondArr) {
 
 const letterPositions = function(sentence) {
   const results = {};
-
-
+  const seen = []
+    for (let i = 0; i < sentence.length; i++) {
+       if (results[sentence[i]]) {
+         results[sentence[i]].push(i)
+       }else{
+         results[sentence[i]] = [i]
+       }       
+    }
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
