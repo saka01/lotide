d435b79f5474c80f6b70164fbe09a0a08aec3c84
Saka Moshood <sakamoshood@ymail.com>
2021-05-31 18:09:52 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,18 @@
+const findKey = (object, callback) => {
+  for(const key in object){
+
+    if (callback(object[key])) {
+      return key
+    }
+  }
+  return "Not Found"
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
