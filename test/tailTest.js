e9e2953bd037f36687ddbc8e75b75a2791de4212
Saka Moshood <sakamoshood@ymail.com>
2021-06-01 17:23:54 +0000

    added functions to index.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tailTest.js", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
