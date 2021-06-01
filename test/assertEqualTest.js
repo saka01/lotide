5d9af05b9c9f626f3bae363a61dc4d034437b56c
Saka Moshood <sakamoshood@ymail.com>
2021-06-01 00:13:44 +0000

    started writing assertEqual tests

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+
+
---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+// const assert = require('chai').assert;
+const head = require('../head');
+
+// TEST CODE
+
+head("Lighthouse Labs", "Bootcamp");
+
+head(1, 1);
+
